954137ca37f3bfc91416e0fe6eac61f28004fb00
Tyler Burgess <tyster23@gmail.com>
2015-09-29 08:43:47 -0600

    Array Sum
    Got the Array working

--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,31 @@
 'use strict';
 
 function longestString(arr) {
+  var longest;
+
+  // Loop Method
+  for (var i=0; i<arr.length; i++) {
+    var elem = arr[i];
+    if (typeof elem === 'string') {
+      if (!longest || elem.length > longest.length) {
+        longest = elem;
+      }
+    }
+  }
+
+  return longest;
 
 }
+
+  // Sort Method
+//   return arr.sort(function (a, b) {
+//     if (typeof a !== 'string') { return 1; }
+//     if (typeof b !== 'string') { return -1; }
+//     if (a.length > b.length) { return -1; }
+//     if (b.length > a.length) { return 1; }
+//     return 0;
+//     }
+//   })[0]
+// }
 
 module.exports = longestString;
